122b7c2516494940ff9f9d3431fcea086e9e38ca
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-05 12:28:42 +0900

    feat: solve 46
    45 번 최대값을 최솟값으로 바꾸면 됨.
    
    but, 초기값을 너주는거 잊지 말기

--- 46_16194.js
+++ 46_16194.js
@@ -1,10 +1,25 @@
 // const fs = require("fs");
 // let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const input = `4
-1 5 6 7`.split('\n');
+3 5 15 16`.split('\n');
+
+const N = Number(input[0]);
+const cards = [0, ...input[1].split(' ').map(Number)];
 
 function sol() {
+	let d = new Array(N+1).fill(0);
 
+	for (let i=1; i<=N; i++) {
+		for (let j=1; j<=i; j++) {
+			if (d[i] === 0) {
+				d[i] = d[i-j] +cards[j];
+				continue;
+			}
+			d[i] = Math.min(d[i], d[i-j] + cards[j]);
+		}
+	}
+
+	return d[N];
 }
 
 
