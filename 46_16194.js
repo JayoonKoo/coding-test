7fbede230df099f56270cc1f65b913cb29a7979e
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-05 12:10:44 +0900

    feat: create 46

---
+++ 46_16194.js
@@ -0,0 +1,11 @@
+// const fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = `4
+1 5 6 7`.split('\n');
+
+function sol() {
+
+}
+
+
+console.log(sol(input));
