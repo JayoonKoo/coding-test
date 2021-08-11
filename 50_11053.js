73d6250accc3a3307eab6caeca81ca69c5c2a985
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-11 21:49:26 +0900

    feat: create 50

---
+++ 50_11053.js
@@ -0,0 +1,10 @@
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = `2`.split('\n');
+
+
+function sol() {
+
+}
+
+console.log(sol());
