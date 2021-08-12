9105667d11da192ce6fa6257d5cfaf5ea142e37d
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-12 19:29:05 +0900

    feat: solve 50

--- 50_11053.js
+++ 50_11053.js
@@ -1,10 +1,23 @@
 // const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const input = `2`.split('\n');
+const input = `6
+10 20 10 30 20 50`.split('\n');
 
+const N = Number(input[0]);
+const A = input[1].split(' ').map(Number);
 
 function sol() {
+	let d = new Array(N).fill(1);
 
+	for (let i=0; i<N; i++) {
+		const target = A[i];
+		for (let j=0; j<i; j++) {
+			if (target <= A[j]) continue;
+			if (d[i] < d[j] + 1) d[i] = d[j] + 1;
+		}
+	}
+
+	return Math.max(...d);
 }
 
 console.log(sol());
