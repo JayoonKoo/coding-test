c906c10578ab6ea79c8c442b9c13dfdd25e565c8
JayoonKoo <rnwkdbs12@gmail.com>
2021-08-21 00:50:00 +0900

    feat: solve 52

---
+++ 52_1912.js
@@ -0,0 +1,20 @@
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let input = `5
+-1 -2 -3 -4 -5`.split('\n');
+
+const N = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+
+const sol = () => {
+	const d = [arr[0]];
+	for (let i=1; i <N; i++) {
+		const [current, preD, preArr ] = [arr[i], d[i-1], arr[i-1]];
+		const candidate = [preD + current, current, current + preArr];
+		d[i] = Math.max(...candidate);
+	}
+
+	return Math.max(...d);
+}
+
+console.log(sol());
